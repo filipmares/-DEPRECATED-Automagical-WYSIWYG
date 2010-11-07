d2ce1ee99274a1e03fb9201690157065909ee120
Filip Mares <filipmares@gmail.com>
2010-11-06 23:53:17 -0400

    Made menu work

--- src/initNew.js
+++ src/initNew.js
@@ -4,31 +4,29 @@ if( !builder ) {
 }
 
 builder.init = (function()
-{	
-	var init = {}, populateNavList;
-	
+{   
+    var init = {}, populateNavList;
+    
     populateNavList = function()
     {
-    	$.getJSON('Selectors/index.json',function(json, status){
-			var folderName = "";
-    		for(var i = 0; i < json.main.selectors.length;i++)
-    		{
-    			folderName = json.main.selectors[i].Folder_Name;
-    			$("nav#nav").append('<a id="'+ folderName + '" href="#">' + folderName + '</a>');
-    		}
-    	});
+        $.getJSON('Selectors/index.json',function(json, status){
+            $.each(json.main.selectors, function(name, cell){
+                $("nav#nav").append('<a id="'+ cell.Folder_Name + '" href="#">' + cell.Folder_Name + '</a>');
+               $('a#'+cell.Folder_name).click(function(){
+               
+                   $("nav#selector").slideToggle("slow"); 
+                   $('a#'+cell.Folder_name).addClass("active");
+               });
+            });
+        });
     };
      
     init.initialize = function()
     {
 
-		populateNavList();
+        populateNavList();
     };
     
     return init;
     
 }());
-
-
-
-
