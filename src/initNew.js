e18daacb8b59a52b3a3add724cb169d7ca24a6e4
Filip Mares <filipmares@gmail.com>
2010-11-07 01:45:51 -0500

    Added comments to initNew.js

--- src/initNew.js
+++ src/initNew.js
@@ -11,16 +11,23 @@ builder.init = (function ()
 	{
 	},
 		populateNavList;
+		
+	/*This funtion populates the MenuBar and Selector bar with extensions and their various cells*/	
 	populateNavList = function ()
 	{
+		//Get INDEX JSON file to iterate through extensions
 		$.getJSON('Selectors/index.json', function (json, status)
 		{
+			//Iterate Through extensions
 			$.each(json.main.selectors, function (name, cell)
 			{
-				console.log(cell.Folder_Name);
+				console.log("Found Folder: " + cell.Folder_Name);
+				//Populate MenuBar with extension folder names.
 				$("nav#nav").append('<a id="' + cell.Folder_Name + '" href="#">' + cell.Folder_Name + '</a>');
+				//Add on-click behaviour to MenuBar Items.
 				$('a#' + cell.Folder_Name).click(function ()
 				{
+					//If this menu item is active then remove active class and hide SelectorBar.
 					if ($(this).hasClass('active'))
 					{
 						$(".active").removeClass("active");
@@ -28,6 +35,8 @@ builder.init = (function ()
 					}
 					else
 					{
+					//Else if any MenuBar item is acitve, remove active class and hide menu.
+					//Make item clicked active and slide SelectorBar.
 						if ($("nav#selector").is(":visible"))
 						{
 							$("nav#selector").hide();
