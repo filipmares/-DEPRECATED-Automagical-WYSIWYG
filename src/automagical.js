e8f6536137351186ee06355c2b6f980dc467ea1c
@deniszgonjanin <deniszgonjanin@gmail.com>
2011-01-04 02:12:50 -0500

    Refactored the Get HTML functionality into a separate source file

--- src/automagical.js
+++ src/automagical.js
@@ -29,70 +29,11 @@ builder.init = (function ()
     	return $('<div>').append( element.eq(0).clone() ).html();
 	};
 	
-	/* This function initializes the GetHtml link button*/
-	initializeGetHtml = function() {
-	
-			$('a#getHTML').click(function(){
-					
-					var canvasHTML="";
-					
-					//We need to recursively go through and do element by element for a multitude of reasons right now
-					$('#canvas').children().each(function() {
-					
-						canvasHTML = recursiveHTMLAppendFunction(canvasHTML, $(this).clone())
-
-    				});
-    		
-					
-					var allHTML = $.ajax({async:false, url:'template.html',}).responseText;
-					allHTML = allHTML.replace(/\{body\}/m, canvasHTML);
-					
-					
-
-					//Output HTML to console
-					console.log(allHTML);
-
-			});
-	};
-
-	/* This function recursively iterates through all of an element's children to produce it's html */
-    recursiveHTMLAppendFunction = function( canvasHTML, element) {
-		
-		//if the div is only being used for resizing purposes, don't include it in final html
-		if ((element.filter('.ui-resizable-handle, .ui-resizable-e, .ui-resizable-handle, .ui-resizable-s, .ui-resizable-handle, .ui-resizable-se, .ui-icon, .ui-icon-gripsmall-diagonal-se')).size() > 0) {
-			return "";
-		}
-		
-		//remove all the classes we are using for editing purposes
-		element.removeClass('component container ui-draggable added ui-resizable ');
-
-		//TODO: This doesn't take into acount any other attributes other than 'style', this needs to be changed
-		canvasHTML += "<"+ element.get(0).tagName + " " + defineInlineCssProperty(element) + ">" + element.clone().find("*").remove().end().text();
-		
-		//Go through all of the children of this element
-        if (element.children().size() > 0) {
-            
-            element.children().each( function() {
-            	
-                canvasHTML += recursiveHTMLAppendFunction("", $(this));
-            });
-        }
-        
-       	canvasHTML += "</"+ element.get(0).tagName + ">";
-       	
-       	return canvasHTML;
-    };
-    
-    /* This function is used to get all the inline style attributes of an element that is set*/
-    defineInlineCssProperty = function(element) {
-    	
-    	var styleString = "style=\"";
-    	
-    	styleString+= element.attr('style');
-    	
-    	styleString+= "\"";
-    	return styleString;
-    };
+	initializeGetHtml = function(){
+		$('a#getHTML').click(function(){
+			$('#canvas').postProcessing();
+		});
+	}
 
 	
 	hideElements = function () {
